5ba41341cc85ab6c800b10aa8036a14341e90afb
pofieldsilence <polina.loktionova@bk.ru>
2021-04-08 13:31:18 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -47,33 +47,34 @@ int countPairs2(int *arr, int len, int value) {
 
 int countPairs3(int *arr, int len, int value) {
   if (len == 0) return 0;
-  int count = 0;
 
-  for (int i = 0; i < len; i++) {
-    int left = i;
-    int right = len - 1;
-    int midIndex = 0;
-    while (left <= right) {
-      midIndex = (left + right) / 2;
-      int sum = arr[midIndex] + arr[i];
-      if (sum == value) {
-        break;
-      }
-      if (value < sum) {
-        right = midIndex - 1;
-      } else {
-        left = midIndex + 1;
-      }
+  int count = 0, start = 0, end = len - 1;
+    while (start < end - 1) {
+        int mid1 = (start + end) / 2;
+        if (arr[mid1] <= value)
+            start = mid1;
+        else
+            end = mid1;
     }
-    while (arr[i] + arr[midIndex] == value) {
-      midIndex--;
-    }
-    midIndex++;
-    while (arr[i] + arr[midIndex] == value) {
-      count++;
-      midIndex++;
-    }
-  }
+    len = end - 1;
 
-  return count;
+    for (int i = 0; i <len; i++) {
+        start = i+1, end = len-1;
+        int cNum = 0;
+
+        while (start < end) {
+            int mid2 = (start + end) / 2;
+            if (arr[mid2] < (value-arr[i]))
+                start = mid2 + 1;
+            else
+                end = mid2;
+        }
+
+        while (arr[start] == (value - arr[i])) {
+            cNum++;
+            start++;
+        }
+        count += cNum;
+    }
+    return count;
 }
